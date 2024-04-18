6484dbef479e121161f07552bc507a2a1b71b713
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-18 18:27:08 -0300

    req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,11 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("Tomate")
+    ingredient_2 = Ingredient("Tomate")
+    assert ingredient_1.name == "Tomate"
+    assert ingredient_1.restrictions == set()
+    assert hash(ingredient_1) == hash("Tomate")
+    assert ingredient_1 == ingredient_2
+    assert repr(ingredient_1) == "Ingredient('Tomate')"
